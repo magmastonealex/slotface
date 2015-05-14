f305a3adafd2dd3e5858b5337076c1e1ab705f39
Alex Roth <me@magmastone.net>
2015-05-14 18:57:37 -0400

    Making progress on proper transparency

--- src/slotface.c
+++ src/slotface.c
@@ -2,33 +2,52 @@
 
 static Window *window;
 static TextLayer *text_layer;
+static InverterLayer *iLayer1;
+static Layer *faceLayer;
+static GBitmap * map;
+static BitmapLayer * bLayer;
 
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
-}
+static void my_layer_draw(Layer *layeer, GContext *ctx) {
+ graphics_context_set_compositing_mode(ctx,GCompOpClear);
+ graphics_context_set_fill_color(ctx,GColorWhite);
+ graphics_fill_rect(ctx,GRect(0,0,144,168),0,GCornerNone);
+ graphics_context_set_fill_color(ctx,GColorBlack);
+ graphics_context_set_stroke_color(ctx,GColorBlack);
+ graphics_fill_circle(ctx,GPoint(40,30),40);
 
-static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
-}
+// map=graphics_capture_frame_buffer(ctx);
+ //bitmap_layer_set_bitmap(bLayer,map);
+// graphics_release_frame_buffer(ctx,map);
 
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
-}
-
-static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+ graphics_context_set_fill_color(ctx,GColorWhite);
+ graphics_fill_rect(ctx,GRect(0,0,144,168),0,GCornerNone);
 }
 
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  iLayer1=inverter_layer_create(GRect(0, 0, 144, 168));
+  text_layer=text_layer_create(GRect(40, 30, 90, 100));
+  text_layer_set_text(text_layer, "10");
+  text_layer_set_background_color(text_layer,GColorClear);
+  text_layer_set_text_color(text_layer,GColorWhite);
+  
+
+  faceLayer=layer_create(GRect(0, 0, 144, 168));
+
+  layer_set_update_proc(faceLayer, my_layer_draw);
+       
+//  map= __gbitmap_create_blank(GSize(144,168));
+  map=gbitmap_create_with_resource(RESOURCE_ID_IMAGE_TEMP);
+  bLayer = bitmap_layer_create(GRect(0,0,144,168));
+  bitmap_layer_set_compositing_mode(bLayer,GCompOpSet);
+  bitmap_layer_set_bitmap(bLayer,map);
+  layer_add_child(window_layer, faceLayer);
+
+  layer_add_child(faceLayer,inverter_layer_get_layer(iLayer1));
+
+  layer_add_child(inverter_layer_get_layer(iLayer1),text_layer_get_layer(text_layer));
+  layer_add_child(inverter_layer_get_layer(iLayer1),bitmap_layer_get_layer(bLayer));
 }
 
 static void window_unload(Window *window) {
@@ -37,7 +56,6 @@ static void window_unload(Window *window) {
 
 static void init(void) {
   window = window_create();
-  window_set_click_config_provider(window, click_config_provider);
   window_set_window_handlers(window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload,
